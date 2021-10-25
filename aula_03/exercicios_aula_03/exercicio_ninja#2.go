54e93b829e0af2c1170666b2644759c11a01d49b
opauwlo <opvcfreitas@gmail.com>
2021-10-25 17:49:59 -0300

    exercicio 2, tipos e package level

---
+++ aula_03/exercicios_aula_03/exercicio_ninja#2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+var x int 
+var y string
+var z bool
+
+func 
+
+func main()  {
+
+  fmt.Println("exercicio 2, package level and tipos")
+  fmt.Println(x, y, z) //valor zero
+}
