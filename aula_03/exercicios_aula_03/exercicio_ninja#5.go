25c854f71ea57d850d10682fe2a2147d3c0f358d
opauwlo <opvcfreitas@gmail.com>
2021-10-25 19:46:05 -0300

    exercicio 5, tipos personalizados e conversão de tipo

---
+++ aula_03/exercicios_aula_03/exercicio_ninja#5.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+)
+
+type True int
+
+var(
+  verdade True
+  y int
+)
+func main() {
+  fmt.Println("\nExercicio 5, tipos personalizados e conversion")
+  fmt.Println("==========================================\n")
+  fmt.Printf("%v \t%T\n", verdade, verdade)
+  fmt.Printf("%v \t%T\n", y, y)
+
+  verdade = 42
+  y = int(verdade)
+  fmt.Printf("%v \t%T\n", verdade, verdade)
+  fmt.Printf("%v \t%T\n", y, y)
+
+}
