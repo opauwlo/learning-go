3a52a36c57f11a0efc9af69f7913fca4f5a082f2
opauwlo <opvcfreitas@gmail.com>
2021-10-25 13:47:00 -0300

    add aula_02

--- aula_01/aula_01.go
+++ aula_01/aula_01.go
@@ -16,9 +16,9 @@ func tips_auto() {
 func tips_fixed() {
   // aqui as vars são tipadas por mim e seu tipo não pode ser alterado
   // o uso do sufixo "var" indica que essa var pode ser acessada em qualquer parte do package, semelhante ao var vs let em js, o:= é let e var é var
- var age int = 22
- var name string = "pauwlo"
- var live bool = true
+  var age int = 22
+  var name string = "pauwlo"
+  var live bool = true
 
   fmt.Println(age, name, live)
 }
---
+++ aula_02/aula_02.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "fmt"
+)
+
+func func_with_params(age int, name string, live bool) {
+  fmt.Println(age, name, live)
+}
+
+func for_star_ex() {
+  for i:=1; i<=5; i++ {
+    for j:=5; j>=i; j-- {
+      fmt.Print("*")
+    }
+    fmt.Print("\n")
+  }
+}
+
+func for_star2_ex() {
+  for i:=5; i>=1; i-- {
+    for j:=1; j<=i; j++ {
+      fmt.Print("*")
+    }
+    fmt.Print("\n")
+  }
+}
+
+func main() {
+  fmt.Println("aula 02\nrec\n")
+  // func_with_params(21, "pauwlo", true)
+  for_star_ex()
+  for_star2_ex()
+}
