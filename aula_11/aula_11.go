e79a73550406512634fc8fcf82ee6d01afa17c2c
opauwlo <opvcfreitas@gmail.com>
2021-10-31 10:30:57 -0300

    Aula 11, Deslocamento de bits

---
+++ aula_11/aula_11.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  fmt.Println("Aula 10, Deslocamento de BITSssssssss ")
+  
+  x := 10
+  y := x << 1
+  z := x >> 1
+  fmt.Printf("%b\n", x)
+  fmt.Printf("%b\n", y)
+  fmt.Printf("%b\n", z)
+}
