b5e9a7bb3c636ea487b88ae2609d5024e70cd990
opauwlo <opvcfreitas@gmail.com>
2021-11-01 11:44:03 -0300

    Aula 12. Exercicio 05

---
+++ aula_12/exercicios/exercicio_ninja#5.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+/*
+ *Crie uma variável de tipo string utilizando uma raw string literal.
+ *Demonstre-a.
+*/
+
+func main() {
+  fmt.Printf("exercicio 05\n")
+  text := `Será que um dia o dia a dia será normal?
+                           o dia`
+  fmt.Println(text)
+}
