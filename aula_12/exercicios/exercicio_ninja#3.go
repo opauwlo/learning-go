ffc4fcb4ef6b48cc10e09e6683e1b627d97aa172
opauwlo <opvcfreitas@gmail.com>
2021-11-01 10:50:46 -0300

    Aula 12. Exercicio 03

---
+++ aula_12/exercicios/exercicio_ninja#3.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+/*
+ * Crie constantes tipadas e não-tipadas.
+ * Demonstre seus valores.
+ * 
+ */
+const x int = 10
+const y =  10
+func main() {
+  fmt.Println("exercicio 3")
+  fmt.Println(x, y)
+}
